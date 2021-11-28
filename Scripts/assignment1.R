e92679779dc61aef731e3a8772ba656335cf4b3d
Brian McClune <bpmcclune@gmail.com>
2021-11-28 13:17:10 -0500

    Complete part 2 of assignment 1

--- Scripts/assignment1.R
+++ Scripts/assignment1.R
@@ -7,3 +7,18 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     df <- do.call("rbind", dfs)
     mean(df[pollutant][!is.na(df[pollutant])])
 }
+
+complete <- function(directory, id = 1:332) {
+    dfs <- vector(mode = "list", length = length(id))
+    for (value in seq_along(id)) {
+        csv <- file.path(directory, sprintf("%03d.csv", id[[value]]))
+        df <- read.csv(csv)
+        dfs[[value]] <- data.frame(
+            id = id[[value]],
+            nobs = sum(complete.cases(df))
+        )
+    }
+    res <- do.call("rbind", dfs)
+    colnames(res) <- c("id", "nobs")
+    res
+}
