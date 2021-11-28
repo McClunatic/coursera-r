64a333fbd97cf430d94855bfe894e27fd47da783
Brian McClune <bpmcclune@gmail.com>
2021-11-28 07:50:05 -0500

    Add Roger Peng initial demo functions

--- testing.R
+++ testing.R
@@ -1,3 +1,12 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+my_function <- function() {
+    x <- rnorm(100)
+    mean(x)
+}
+
+second <- function(x) {
+    x <- x + rnorm(length(x))
+}
